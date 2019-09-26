0c8d60803213bf264d73318240b8fe1b4034fe07
Haeldeus <nordein.vail@yahoo.de>
2019-09-26 02:45:13 +0200

    Libs added, First Try at Excel Sheets
    - Added the Apache POI Library to work with Excel Sheets.
    - First version of an Excel Table implemented when pressing the Export
    Button.
    - Added a Template to the Folder for further Testing with the Excel
    Sheets

--- CashAssets/src/kasse/ExportButtonListener.java
+++ CashAssets/src/kasse/ExportButtonListener.java
@@ -1,7 +1,15 @@
 package kasse;
 
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * The ActionListener for the Export-Button.
@@ -10,10 +18,28 @@ import java.awt.event.ActionListener;
  */
 public class ExportButtonListener implements ActionListener {
 
+  private final Container container;
+  
+  public ExportButtonListener(Container c) {
+    this.container = c;
+  }
+  
+
   @Override
   public void actionPerformed(ActionEvent arg0) {
-    // TODO Auto-generated method stub
-
+    // TODO Create a new Excel File from Scratch.
+    XSSFWorkbook workbook = new XSSFWorkbook();
+    XSSFSheet sheet = workbook.createSheet("Current Date"); //TODO add current Date to Sheet-Title
+    XSSFRow row = sheet.createRow(0);
+    XSSFCell cell = row.createCell(0);
+    workbook.setPrintArea(0, 0, 0, 0, 0);
+    cell.setCellValue("Gezählt von:");
+    try {
+      workbook.write(new FileOutputStream("Try.xlsx"));
+      workbook.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
